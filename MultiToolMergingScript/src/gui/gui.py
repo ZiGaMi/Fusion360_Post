7ab59ee695bb8f25dab59efb07a76f6c842160c5
zigam <ziga.miklosic@emsiso.com>
2022-02-10 13:45:44 +0100

    - added GUI sources

--- MultiToolMergingScript/src/gui/gui.py
+++ MultiToolMergingScript/src/gui/gui.py
@@ -1,14 +1,9 @@
 # ===============================================================================
-# @file:    main.py
+# @file:    gui.py
 # @note:    Fusion360 different tools G-code merging script
 # @author:  Ziga Miklosic
-# @date:    09.02.2022
-# @brief:   This scripts merge together all selected G-code generated files
-#           that are being post separately due to Fusion360 free licence limitation.
-#
-#           End script is result of concatanation of tool paths with different
-#           milling jobs. Start and end of script stays the same only tool 
-#           path section will be copied to the end script.
+# @date:    10.02.2022
+# @brief:   
 # ===============================================================================
 
 # ===============================================================================
@@ -17,23 +12,27 @@
 import sys
 import os
 
-# GUI
-import gui.gui as GUI
+# Kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
 
 # ===============================================================================
 #       CONSTANTS
 # ===============================================================================
 
-# Supported G-code file extension
-SUPPORT_FILE_END = [ ".tap", ".gcode" ]
-
-# Show merge script at end of program
-OPEN_END_SCRIPT = True
-
 
 # ===============================================================================
 #       CLASSES
 # ===============================================================================
+
+
+class MainCanvas(Widget):
+    pass
+
+
+class Application(App):
+    pass
+
 
 
 # ===============================================================================
@@ -45,17 +44,7 @@ OPEN_END_SCRIPT = True
 #
 # @return:       void
 # ===============================================================================
-def main():
-    
-    # Run main application
-    GUI.Application().run()
 
-
-# ===============================================================================
-#       MAIN ENTRY
-# ===============================================================================
-if __name__ == "__main__":
-    main()
 	
 # ===============================================================================
 #       END OF FILE
